63d3328e698f4e02debc4a800935b297b7600f67
Vishnu Varshnay <121924768+Vishnuvarshnay@users.noreply.github.com>
2026-02-25 18:41:15 +0530

    Change CORS origin to production URL

--- backened/index.js
+++ backened/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://virtual-assitant-i5r9.onrender.com",
   credentials:true
 }))
 app.use("/api/auth",authRouter)
